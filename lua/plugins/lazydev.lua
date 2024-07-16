8ae1a36eedae7f644199c87c6cd4de35cdb348a9
Aaron <aaron@sirrom.systems>
2024-07-16 09:37:19 -0600

    handle updates and configure python dap

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -1,25 +1,29 @@
 return {
   {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
+    "mfussenegger/nvim-dap-python",
+    dependencies = {
+      "linux-cultist/venv-selector.nvim",
+    },
+    keys = {
+      {
+        "<leader>dPt",
+        function()
+          require("dap-python").test_method({ config = { justMyCode = false } })
+        end,
+        desc = "Debug Method",
+        ft = "python",
+      },
+      {
+        "<leader>dPc",
+        function()
+          require("dap-python").test_class({ config = { justMyCode = false } })
+        end,
+        desc = "Debug Class",
+        ft = "python",
       },
     },
-  },
-  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-  { -- optional completion source for require statements and module annotations
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = "lazydev",
-        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-      })
+    config = function()
+      require("dap-python").setup(require("venv-selector").python())
     end,
   },
-  { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }
