d4e0bec749aa7c95f67ce610771429fbbb08c039
Aaron <aaron@sirrom.systems>
2024-09-10 14:24:28 -0600

    install cmake extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
     -- import/override with your plugins
     { import = "plugins" },
   },
